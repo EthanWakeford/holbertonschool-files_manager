ed557c66cbfa4553ce17d652b4e6ef693cf889a3
CalebHouse99 <calebhouse99@gmail.com>
2023-07-24 23:07:41 +0000

    returns id as string now

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -29,7 +29,7 @@ class UsersController {
 
     const userCreated = await dbClient.client.db().collection('users').insertOne(newUser);
 
-    res.status(201).json({ id: userCreated.insertedId, email });
+    res.status(201).json({ id: userCreated.insertedId.toString(), email });
   }
 }
 
