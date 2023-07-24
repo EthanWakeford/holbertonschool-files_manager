4e1820e6dc4c5c5fcea1ad4c80c0deca9d6130da
CalebHouse99 <calebhouse99@gmail.com>
2023-07-24 22:40:46 +0000

    body parser

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-import dbClient from './utils/db';
+const express = require('express');
+const dbClient = require('./utils/db');
+const router = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/', router);
 
 const waitConnection = () => {
     return new Promise((resolve, reject) => {
@@ -20,3 +28,14 @@ const waitConnection = () => {
         repeatFct();
     })
 };
+
+waitConnection()
+    .then(() => {
+        app.listen(5000, () => {
+            console.log('Server is running at port 5000');
+        });
+    })
+    .catch(() => {
+        console.error('Cannot connect to database');
+        process.exit(1);
+    });
