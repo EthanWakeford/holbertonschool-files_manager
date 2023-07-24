75d8bcf0b60710d824c60643f88d746f09fa436a
CalebHouse99 <calebhouse99@gmail.com>
2023-07-24 22:21:33 +0000

    task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,36 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+      return;
+    }
+
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const user = await dbClient.client.db().collection('users').findOne({ email });
+    if (user) {
+      res.status(400).json({ error: 'Already exist' });
+      return;
+    }
+
+    const hashedPassword = sha1(password);
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    const userCreated = await dbClient.client.db().collection('users').insertOne(newUser);
+
+    res.status(201).json({ id: userCreated.insertedId, email });
+  }
+}
+
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 
 router.get('/status', AppController.status);
 router.get('/stats', AppController.stats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
