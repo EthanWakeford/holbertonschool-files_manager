85a0123c04244ba1e27c794644219ec2e5a54d3a
EthanWakeford <ethanwakeford@gmail.com>
2023-07-25 15:01:16 -0500

    added task 6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -18,10 +18,14 @@ class FilesController {
     if (!['folder', 'file', 'image'].includes(type)) return res.status(400).json({ error: 'Missing type' });
     if (!data && type !== 'folder') return res.status(400).json({ error: 'Missing data' });
     if (parentId !== 0) {
-      const file = dbClient.db.collection('files').findOne({ parentId });
+      const project = new mongo.ObjectID(parentId);
+      const file = await dbClient.db
+        .collection('files')
+        .findOne({ _id: project });
 
       if (!file) return res.status(400).json({ error: 'Parent not found' });
-      if (file.type !== 'folder') return res.status(400).json({ error: 'Parent is not a folder' });
+
+      if (file && file.type !== 'folder') return res.status(400).json({ error: 'Parent is not a folder' });
     }
 
     let newFile;
@@ -63,6 +67,70 @@ class FilesController {
       isPublic,
       parentId,
     });
+  }
+
+  static async getShow(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    const { id } = req.params;
+    const file = await dbClient.db
+      .collection('files')
+      .findOne({ _id: new mongo.ObjectId(id) });
+
+    if (!file) return res.status(404).json({ error: 'Not found' });
+    if (userId !== file.userId.toString()) return res.status(404).json({ error: 'Not found' });
+
+    return res.status(200).send({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    });
+  }
+
+  static async getIndex(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    const { parentId, page = 0 } = req.params;
+
+    let files;
+
+    if (parentId) {
+      files = await dbClient.db
+        .collection('files')
+        .aggregate([
+          { $match: { parentId: new mongo.ObjectID(parentId) } },
+          { $skip: page * 20 },
+          { $limit: 20 },
+        ])
+        .toArray();
+    } else {
+      files = await dbClient.db
+        .collection('files')
+        .aggregate([
+          { $match: { userId: new mongo.ObjectID(userId) } },
+          { $skip: page * 20 },
+          { $limit: 20 },
+        ])
+        .toArray();
+    }
+
+    const returnFile = files.map((file) => ({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    }));
+
+    return res.status(200).send(returnFile);
   }
 }
 
