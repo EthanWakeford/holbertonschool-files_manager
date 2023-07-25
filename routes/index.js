35e8f94a2e48d981ebb291bb0515bd3c8d1c523a
CalebHouse99 <calebhouse99@gmail.com>
2023-07-25 01:11:27 +0000

    go back go back go back

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
+router.get('/status', AppController.status);
+router.get('/stats', AppController.stats);
 
 module.exports = router;
